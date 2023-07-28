8fc47eae65cd1a442b3e181952c2eece0e36723d
kubra <kubracetin.ist@gmail.com>
2023-07-28 17:42:31 -0400

    controller

---
+++ src/main/java/com/example/controller/ChatController.java
@@ -0,0 +1,21 @@
+package com.example.controller;
+
+import com.example.model.Message;
+import com.example.service.OpenAIService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatController {
+
+    @Autowired
+    OpenAIService service;
+
+    @GetMapping("/chat")
+    public Message getChat(@RequestParam String prompt){
+        return service.chat(prompt);
+    }
+
+}
--- src/main/java/com/example/service/OpenAIService.java
+++ src/main/java/com/example/service/OpenAIService.java
@@ -1,6 +1,15 @@
 package com.example.service;
 
+import com.example.model.ChatRequestDto;
+import com.example.model.ChatResponseDto;
+import com.example.model.Message;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
 
 public class OpenAIService {
 
@@ -14,6 +23,18 @@ public class OpenAIService {
     @Value("${API_MODEL}")
     private String model;
 
+   private RestTemplate template = new RestTemplate();
 
+   public Message chat(String prompt){
+       HttpHeaders headers = new HttpHeaders();
+       headers.setContentType(MediaType.APPLICATION_JSON);
+       headers.setBearerAuth(apiKey);
+
+       ChatRequestDto request = new ChatRequestDto(model, prompt);
+       HttpEntity<ChatRequestDto> entity = new HttpEntity<>(request, headers);
+
+       ResponseEntity<ChatResponseDto> response = template.postForEntity(url, entity, ChatResponseDto.class);
+       return response.getBody().getChoices().get(0).getMessage();
+   }
 
 }
