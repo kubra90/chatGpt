efd069eeba88a398595722750f20c93b3cc506f0
kubra <kubracetin.ist@gmail.com>
2023-07-28 17:27:04 -0400

    model

--- src/main/java/com/example/model/Choice.java
+++ src/main/java/com/example/model/Choice.java
@@ -1,4 +1,31 @@
 package com.example.model;
 
 public class Choice {
+
+    private int index;
+    private Message message;
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public Message getMessage() {
+        return message;
+    }
+
+    public void setMessage(Message message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "Choice{" +
+                "index=" + index +
+                ", message=" + message +
+                '}';
+    }
 }
