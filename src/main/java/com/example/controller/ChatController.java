8187304b0e7d957c62356a3f7d114b4b06ce239e
kubra <kubracetin.ist@gmail.com>
2023-08-01 00:13:09 -0400

    add features

--- src/main/java/com/example/controller/ChatController.java
+++ src/main/java/com/example/controller/ChatController.java
@@ -7,15 +7,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class ChatController {
 
     @Autowired
     OpenAIService service;
 
+//    @GetMapping("/chat")
+//    public Message getChat(@RequestParam String prompt){
+//        return service.chat(prompt);
+//    }
+
     @GetMapping("/chat")
-    public Message getChat(@RequestParam String prompt){
-        return service.chat(prompt);
+    public List<Message> getChat(){
+        return service.chat();
     }
 
 }
--- src/main/java/com/example/service/OpenAIService.java
+++ src/main/java/com/example/service/OpenAIService.java
@@ -2,6 +2,7 @@ package com.example.service;
 
 import com.example.model.ChatRequestDto;
 import com.example.model.ChatResponseDto;
+import com.example.model.FileReader;
 import com.example.model.Message;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
@@ -10,6 +11,10 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Service
@@ -26,17 +31,36 @@ public class OpenAIService {
     private String model;
 
    private RestTemplate template = new RestTemplate();
+   FileReader file= new FileReader("prompt.csv");
 
-   public Message chat(String prompt){
-       HttpHeaders headers = new HttpHeaders();
-       headers.setContentType(MediaType.APPLICATION_JSON);
-       headers.setBearerAuth(apiKey);
+//   public Message chat(String prompt){
+//       HttpHeaders headers = new HttpHeaders();
+//       headers.setContentType(MediaType.APPLICATION_JSON);
+//       headers.setBearerAuth(apiKey);
+//
+//       ChatRequestDto request = new ChatRequestDto(model, prompt);
+//       HttpEntity<ChatRequestDto> entity = new HttpEntity<>(request, headers);
+//
+//       ResponseEntity<ChatResponseDto> response = template.postForEntity(url, entity, ChatResponseDto.class);
+//       return response.getBody().getChoices().get(0).getMessage();
+//   }
 
-       ChatRequestDto request = new ChatRequestDto(model, prompt);
-       HttpEntity<ChatRequestDto> entity = new HttpEntity<>(request, headers);
+    List<Message> output = file.readingFile();
 
-       ResponseEntity<ChatResponseDto> response = template.postForEntity(url, entity, ChatResponseDto.class);
-       return response.getBody().getChoices().get(0).getMessage();
-   }
+    public List<Message> chat(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setBearerAuth(apiKey);
+        List<Message> result =new ArrayList<>();
+        for(int i=0; i< output.size(); i++) {
+            ChatRequestDto request = new ChatRequestDto(model, "Can you summarize the following text with one word " + (output.get(i)));
+            HttpEntity<ChatRequestDto> entity = new HttpEntity<>(request, headers);
+
+            ResponseEntity<ChatResponseDto> response = template.postForEntity(url, entity, ChatResponseDto.class);
+            result.add(response.getBody().getChoices().get(0).getMessage());
+
+        }
+        return result;
+    }
 
 }
