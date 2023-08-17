958db5c1fd7039142bfa199bff007a4c4c7894b7
kubra <kubracetin.ist@gmail.com>
2023-08-17 13:56:00 -0400

    update

--- src/test/java/com/example/OpenAIServiceApiTest.java
+++ src/test/java/com/example/OpenAIServiceApiTest.java
@@ -1,6 +1,8 @@
 package com.example;
 
+import com.example.model.Message;
 import com.example.service.OpenAIService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,10 +11,18 @@ import org.springframework.boot.test.web.client.TestRestTemplate.HttpClientOptio
 import org.springframework.boot.test.web.client.TestRestTemplate.HttpClientRequestCallback;
 import org.springframework.boot.test.web.client.TestRestTemplate.HttpClientResponseExtractor;
 import org.springframework.boot.test.web.client.TestRestTemplate.HttpClientResponseErrorHandler;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
+
+import javax.imageio.IIOException;
+
+import java.io.IOException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -24,22 +34,23 @@ public class OpenAIServiceApiTest {
     private TestRestTemplate restTemplate;
 
     @Autowired
-    private OpenAIService openAiService
+    private OpenAIService openAiService;
+
+    private MockMultipartFile mockFile;
+
+   @BeforeEach
+   public void setup() throws IOException {
+       //create a mock csv file for testing
+       ClassPathResource resource =new ClassPathResource("test.csv");
+       mockFile = new MockMultipartFile(
+               "file",
+               resource.getInputStream()
+       );
+    }
 
     @Test
     public void testChatApi() {
-        // Define your request data here
-        // ...
-
-        ResponseEntity<String> responseEntity = restTemplate.exchange(
-                "/api/chat", // The API endpoint
-                HttpMethod.POST, // HTTP method
-                null, // Request entity or body
-                String.class // Expected response type
-        );
-
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        // Perform additional assertions on the response
+       List<Message> result = openAiService.chat(mockFile);
     }
 }
 
