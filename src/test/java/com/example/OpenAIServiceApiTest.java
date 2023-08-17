c51fb1e9f649c171bd1e76b9bc6540f38b4ba14d
kubra <kubracetin.ist@gmail.com>
2023-08-17 15:10:05 -0400

    test case

--- src/test/java/com/example/OpenAIServiceApiTest.java
+++ src/test/java/com/example/OpenAIServiceApiTest.java
@@ -8,19 +8,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.client.TestRestTemplate.HttpClientOption;
-import org.springframework.boot.test.web.client.TestRestTemplate.HttpClientRequestCallback;
-import org.springframework.boot.test.web.client.TestRestTemplate.HttpClientResponseExtractor;
-import org.springframework.boot.test.web.client.TestRestTemplate.HttpClientResponseErrorHandler;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
 
 import javax.imageio.IIOException;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.List;
 
@@ -41,16 +41,24 @@ public class OpenAIServiceApiTest {
    @BeforeEach
    public void setup() throws IOException {
        //create a mock csv file for testing
-       ClassPathResource resource =new ClassPathResource("test.csv");
+       File file = new File("test.csv");
+       FileInputStream inputStream = new FileInputStream(file);
+       //constructor for MockMultiPartFile, one is (String name, originalFileName, String contentType, InputStream contentStream)
        mockFile = new MockMultipartFile(
                "file",
-               resource.getInputStream()
+               file.getName(),
+               MediaType.TEXT_PLAIN_VALUE,
+               inputStream
        );
     }
 
     @Test
     public void testChatApi() {
        List<Message> result = openAiService.chat(mockFile);
+
+       //you can add more assertions based on your expected result
+        assertEquals(4, result.size());
+
     }
 }
 
